291fc52c00d90daa8b5ce2a19908a432e46b0257
Stas Versilov <stas.versilov@gmail.com>
2018-04-16 13:27:00 +0400

    Prepare for Hex publishing

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Matrex.MixProject do
     [
       app: :matrex,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
