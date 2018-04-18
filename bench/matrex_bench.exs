f268cb28805233188409a867a8920351b570510a
Stas Versilov <stas.versilov@gmail.com>
2018-04-18 23:46:47 +0400

    Remove unused import. Update stats.

--- bench/matrex_bench.exs
+++ bench/matrex_bench.exs
@@ -18,7 +18,7 @@ defmodule MatrexBench do
 
   ## ExMatrix
   ```
-  Finished in 52.3 seconds
+
 
   benchmark name                iterations   average time
   transpose a 100x100 matrix          5000   763.27 µs/op
@@ -33,24 +33,31 @@ defmodule MatrexBench do
 
   ## Matrex
   ```
-  Finished in 19.3 seconds
+
 
   ## MatrexBench
-  benchmark name                iterations   average time
-  50x50 matrices dot product        500000   6.89 µs/op
-  transpose a 100x100 matrix        100000   28.55 µs/op
-  100x100 matrices dot product       50000   37.50 µs/op
-  200x200 matrices dot product       10000   124.31 µs/op
-  transpose a 200x200 matrix         10000   126.82 µs/op
-  transpose a 400x400 matrix          5000   441.10 µs/op
-  400x400 matrices dot product        5000   673.23 µs/op
-
+  benchmark name                   iterations   average time
+  transpose a 50x50 matrix             500000   3.32 µs/op
+  50x50 matrices dot product           500000   6.61 µs/op
+  transpose a 100x100 matrix           100000   12.47 µs/op
+  300x300 zeros matrix creation        100000   12.99 µs/op
+  300x300 eye matrix creation          100000   14.03 µs/op
+  300x300 fill matrix                  100000   16.69 µs/op
+  100x100 matrices dot product          50000   38.44 µs/op
+  transpose a 200x200 matrix            50000   66.44 µs/op
+  200x200 matrices dot product          10000   135.12 µs/op
+  400x400 matrix sum                    10000   154.72 µs/op
+  transpose a 400x400 matrix            10000   177.85 µs/op
+  400x400 matrices dot product           5000   735.95 µs/op
+  300x300 random matrix transpose        1000   1872.40 µs/op
+  400x400 matrix to list                  500   4417.13 µs/op
+  400x400 matrix to list of lists         500   5357.48 µs/op
+  1000x1000 matrices dot product          200   8669.23 µs/op
   ```
   """
 
   use Benchfella
   import Matrex
-  import MatrexBench.RandomMatrix
 
   @random_a random(50)
   @random_b random(50)
