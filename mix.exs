3de3534e5719b9933c7ee0ca1098045ef59df040
Stas Versilov <stas.versilov@gmail.com>
2018-05-08 17:54:19 +0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Matrex.MixProject do
   def project do
     [
       app: :matrex,
-      version: "0.3",
+      version: "0.3.0",
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
