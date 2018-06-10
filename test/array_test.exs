a618ddc6e1dbdb719c9c79ccc171c31c721c0730
Stas Versilov <stas.versilov@gmail.com>
2018-06-10 22:37:42 +0400

    Move some tests inside types cycle.

--- test/array_test.exs
+++ test/array_test.exs
@@ -37,12 +37,45 @@ defmodule ArrayTest do
       assert c == expected
     end
 
+    test "#reshape changes shape of existing array of #{type}" do
+      a = reshape(1..12, {3, 4})
+      assert shape(a) == {3, 4}
+      r = reshape(a, {2, 6})
+      assert shape(r) == {2, 6}
+      assert at(a, 2, 3) == at(r, 2, 1)
+    end
+
+    test "#reshape creates array from range of #{type}" do
+      a = reshape(1..12, {4, 3}, unquote(type))
+      assert at(a, 3, 2) == 8
+    end
+
+    test "#square squares an array of #{type}" do
+      a = new([1, 2, 3, 4], {2, 2}, unquote(type))
+      expected = new([1, 4, 9, 16], {2, 2}, unquote(type))
+
+      assert square(a) == expected
+    end
+
     test "#sum sums elements of array of #{type}" do
       a = new([1, 2, 3, 4, 5, 6], {3, 2}, unquote(type))
 
       expected = 21
 
       assert sum(a) == expected
+    end
+
+    test "#transpose transposes 2-d array of #{type}" do
+      a = reshape(1..12, {3, 4}, unquote(type))
+      t = transpose(a)
+      assert at(t, 1, 2) == at(a, 2, 1)
+      assert at(t, 1, 3) == at(a, 3, 1)
+      assert at(t, 2, 2) == at(a, 2, 2)
+    end
+
+    test "#zeros creates 3-dim array of #{type}" do
+      a = zeros({10, 28, 28}, unquote(type))
+      assert at(a, {5, 10, 15}) == 0.0
     end
   end)
 
@@ -96,24 +129,6 @@ defmodule ArrayTest do
     assert at(a, {5, 5, 5}) != at(a, {5, 5, 6})
   end
 
-  test "#reshape changes shape of existing array" do
-    a = reshape(1..12, {3, 4})
-    assert shape(a) == {3, 4}
-    r = reshape(a, {2, 6})
-    assert shape(r) == {2, 6}
-    assert at(a, 2, 3) == at(r, 2, 1)
-  end
-
-  test "#reshape creates array from range" do
-    a = reshape(1..9, {3, 3})
-    assert at(a, {3, 3}) == 9
-  end
-
-  test "#reshape creates byte array from range" do
-    a = reshape(1..12, {4, 3}, :byte)
-    assert at(a, 3, 2) == 8
-  end
-
   test "#strides generates strides tuple" do
     assert strides({3, 4}, :float32) == {4 * 4, 4}
     assert strides({10, 5, 5}, :float32) == {25 * 4, 5 * 4, 4}
@@ -126,23 +141,5 @@ defmodule ArrayTest do
 
     assert at(b, {1, 2}) == 1
     assert at(b, {2, 2}) == 3
-  end
-
-  test "#transpose transposes 2-d array" do
-    a = reshape(1..12, {3, 4}, :byte)
-    t = transpose(a)
-    assert at(t, 1, 2) == at(a, 2, 1)
-    assert at(t, 1, 3) == at(a, 3, 1)
-    assert at(t, 2, 2) == at(a, 2, 2)
-  end
-
-  test "#zeros creates 3-dim array of floats" do
-    a = zeros({10, 28, 28}, :float32)
-    assert at(a, {5, 10, 15}) == 0.0
-  end
-
-  test "#zeros creates 3-dim array of doubles" do
-    a = zeros({10, 28, 28}, :float64)
-    assert at(a, {5, 10, 15}) == 0.0
   end
 end
